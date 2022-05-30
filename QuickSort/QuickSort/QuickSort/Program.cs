9a2f19ed1b189224e6a5658d829b765c9d2a2c83
MutazTB <mutazaltbakhi900@gmail.com>
2022-05-31 01:21:07 +0300

    Quick Sort

---
+++ QuickSort/QuickSort/QuickSort/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace QuickSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int[] unsortedArray = { 8, 4, 23, 42, 16, 15 };
+            Console.WriteLine("Unsorted Array:");
+            Print(unsortedArray);
+
+            QuickSort(unsortedArray, 0, unsortedArray.Length - 1);
+            Console.WriteLine("Sorted Array:");
+            Print(unsortedArray);
+
+        }
+        
+        // The recursive part of the algorithm        
+        public static int[] QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                // Partition the Algorithm
+                int position = Partition(array, left, right);
+
+                // Sort the left side
+                QuickSort(array, left, position - 1);
+
+                // Sort the right side
+                QuickSort(array, position + 1, right);
+            }
+            return array;
+
+        }
+
+        // Helper function takes in indices and array of values, iterates through array and swaps values if less than value at right index.        
+        static int Partition(int[] arr, int left, int right)
+        {
+            // set a pivot
+            int pivot = arr[right];
+
+            // get the index of the lower value
+            int low = left - 1;
+
+            for (int i = left; i < right; i++)
+            {
+                if (arr[i] <= pivot)
+                {
+                    low++;
+                    Swap(arr, i, low);
+                }
+
+            }
+
+            Swap(arr, right, low + 1);
+
+            Console.WriteLine(string.Join(",", arr));
+
+
+            return low + 1;
+        }
+
+        /// <summary>
+        /// Swaps two values within an array
+        /// </summary>
+        /// <param name="arr">array of integer values</param>
+        /// <param name="i">index for swap</param>
+        /// <param name="low">other index for swap</param>
+        static void Swap(int[] arr, int i, int low)
+        {
+            int temp;
+            temp = arr[i];
+            arr[i] = arr[low];
+            arr[low] = temp;
+        }
+
+        static void Print(int[] arr)
+        {
+            Console.WriteLine();
+            Console.Write("[");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($" {arr[i]} ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ QuickSort/QuickSort/README.md
@@ -0,0 +1,18 @@
+# Quick Sort
+The Quick Sort is a "Divide and Conquer Algorithm".  It picks a pivot point and moves the values to either the right or the left of it in the array.  It sorts on half and then sorts the other half using recursion and helper functions.  This algortithm is not stable because it does not keep track of the placement/order within the array.
+
+## Challenge
+Create a method named QuickSort that takes an unsorted array.
+Within Main, Output the console the contents of the unsorted array
+- Conduct the quick sort
+- Return the sorted array
+- Output to the console the contents of the sorted array
+
+## Approach & Efficiency
+### Efficiency:
+1. O(n log n) (best)
+2. O(n log n) (avg)
+3. O(n^2) (worst)
+
+## Whiteboard
+![image](images/QuickSort.png)
